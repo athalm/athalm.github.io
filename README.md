39aca3ac0aa85651cc228d3f7fe16b86ad0c3415
Mrunal Athaley <112527910+athalm@users.noreply.github.com>
2024-08-02 19:19:18 -0400

    contact me

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # athalm.github.io
 
 // https://codepen.io/soju22/pen/wvyBorP
+
+
+
+stuff to do
+- the horizontal scroll: if you click and hold, then it should temporarily stop on that frame
+- nav bar at the top left for each page
+- creative stuff too maybe? lol
---
+++ scripts/contact.js
@@ -0,0 +1,25 @@
+emailjs.init('kkycr6R32XSZs6HQJ');
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formData = new FormData(form);
+
+    const data = {
+        name: formData.get('name'),
+        email: formData.get('email'),
+        message: formData.get('message')
+    };
+
+    emailjs.send('service_4nr827j', 'template_ihj4uvo', data)
+        .then(response => {
+            document.getElementById('responseMessage').innerText = 'Message sent successfully!';
+            form.reset();
+        })
+        .catch(error => {
+            document.getElementById('responseMessage').innerText = 'Failed to send message.';
+            console.error('Error:', error);
+        });
+});
+
