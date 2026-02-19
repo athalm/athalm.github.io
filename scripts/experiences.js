ccebfeec4ee459d5fb0a0f421c73eb08e02ea7c2
Mrunal Athaley <112527910+athalm@users.noreply.github.com>
2026-02-19 16:14:10 +0530

    update website

--- scripts/experiences.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".experience-card").forEach((card) => {
-  card.addEventListener("click", () => {
-    document.querySelectorAll(".experience-card").forEach((c) => {
-      c.classList.remove("active");
-    });
-    card.classList.add("active");
-  });
-});
